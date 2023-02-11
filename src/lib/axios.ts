b915ad85f68b5b31d92cb3d773b22deb80422dd5
Rafael Henrique Carneiro <rafael.h@outlook.com>
2023-02-11 00:52:54 -0300

    finish

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,8 +1,8 @@
 import { Axios } from 'axios'
 
-const baseUrl = process.env.BASE_URL
+// const baseUrl = process.env.BASE_URL
 
 export const api = new Axios({
-  baseURL: baseUrl,
+  baseURL: 'https://to-do-backend-psi.vercel.app/',
   headers: { 'Content-type': 'application/json' },
 })
